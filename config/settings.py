26c9679530cac37f4845160c685c441e655a4cd3
Kseniia <127607406+Kseniia-Artemova@users.noreply.github.com>
2023-08-04 16:21:10 +0200

    Develop (#3)
    * set basic settings (#1)
    
    * Creating templates (#2)
    
    * created base templates homepage.html, contact_page.html; added static files; added entry file contact_info.txt
    
    * changed names of controllers

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'catalog',
 ]
 
 MIDDLEWARE = [
@@ -117,7 +119,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ENTRY_PATH = BASE_DIR / 'contact_info.txt'
