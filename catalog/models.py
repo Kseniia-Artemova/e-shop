acab9e8369bb1a8cbff3aaa99a443421a28c8953
Ksu <kseniyaartemova@yandex.ru>
2023-08-08 03:58:15 +0200

    created models Product and Category, created admin, changed settings and urls

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='products/', null=True, blank=True, verbose_name='Изображение')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    creation_date = models.DateField(auto_now_add=True, verbose_name='Дата создания')
+    change_date = models.DateField(auto_now=True, verbose_name='Дата последнего изменения')
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
