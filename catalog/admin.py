4f12228daee17fce9f5f721895e20c09f192425c
Ksu <kseniyaartemova@yandex.ru>
2023-08-08 19:33:29 +0200

    created button for description of product (in admin)

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from catalog.models import Category, Product
+from django.utils.html import format_html
 
 # Register your models here.
 
@@ -11,9 +12,28 @@ class ProductAdmin(admin.ModelAdmin):
         'name',
         'price',
         'category',
+        'toggle_description_button',
     )
     list_filter = ('category', )
     search_fields = ('name', 'description')
+
+    def toggle_description_button(self, obj):
+        return format_html(
+            '<div class="description-wrapper">'
+            '    <button type="button" class="toggle-description">Развернуть</button>'
+            '    <div class="description-content hidden-description">{}</div>'
+            '</div>',
+            obj.description
+        )
+    toggle_description_button.short_description = 'Описание'
+
+    class Media:
+        css = {
+            'all': ('css/admin_description_button.css',)
+        }
+        js = (
+            'js/admin_description_button.js',
+        )
 
 
 @admin.register(Category)
