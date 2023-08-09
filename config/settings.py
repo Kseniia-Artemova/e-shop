f5ba107b00dd965134cce2bdedb6f178c18bc23b
Kseniia <127607406+Kseniia-Artemova@users.noreply.github.com>
2023-08-09 03:01:01 +0200

    Feature/orm (#4)
    * created models Product and Category, created admin, changed settings and urls
    
    * added settings for admin, created custom command, added files with filling for DB
    
    * created button for description of product (in admin)
    
    * created button for description of product (in admin)
    
    * added the ability to set contacts

--- config/settings.py
+++ config/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+PASSWORD_DB = os.getenv('PASSWORD_DB')
 
 
 # Quick-start development settings - unsuitable for production
@@ -77,8 +79,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'catalog',
+        'USER': 'postgres',
+        'PASSWORD': PASSWORD_DB
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -129,3 +133,6 @@ STATICFILES_DIRS = (
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ENTRY_PATH = BASE_DIR / 'contact_info.txt'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
