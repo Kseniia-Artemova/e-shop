5de3c43a59b7dd2550542031edc4856d042f83f7
Kseniia <127607406+Kseniia-Artemova@users.noreply.github.com>
2023-08-09 03:01:01 +0200

    Feature/orm (#4)
    * created models Product and Category, created admin, changed settings and urls
    
    * added settings for admin, created custom command, added files with filling for DB
    
    * created button for description of product (in admin)
    
    * created button for description of product (in admin)
    
    * added the ability to set contacts

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from catalog.models import Category, Product, Contact
+from django.utils.html import format_html
 
 # Register your models here.
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'price',
+        'category',
+        'toggle_description_button',
+    )
+    list_filter = ('category', )
+    search_fields = ('name', 'description')
+
+    def toggle_description_button(self, obj):
+        return format_html(
+            '<div class="description-wrapper">'
+            '    <button type="button" class="toggle-description">Развернуть</button>'
+            '    <div class="description-content hidden-description">{}</div>'
+            '</div>',
+            obj.description
+        )
+    toggle_description_button.short_description = 'Описание'
+
+    class Media:
+        css = {
+            'all': ('css/admin_description_button.css',)
+        }
+        js = (
+            'js/admin_description_button.js',
+        )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')
+    search_fields = ('name', )
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'inn', 'address')
+    search_fields = ('inn', )
