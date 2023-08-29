80fb4572f511ded079f33ec81290ef3263048aab
Kseniia <127607406+Kseniia-Artemova@users.noreply.github.com>
2023-08-29 03:08:50 +0200

    created form for Product (#19)

--- catalog/models.py
+++ catalog/models.py
@@ -28,7 +28,8 @@ class Product(models.Model):
                         )
     price = models.DecimalField(
                             max_digits=8,
-                            decimal_places=2
+                            decimal_places=2,
+                            verbose_name='Стоимость'
                         )
     creation_date = models.DateTimeField(
                             auto_now_add=True,
