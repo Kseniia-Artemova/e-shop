3a6724ab3520fb94f7aaa15a9f2010d1efa0c733
Kseniia <127607406+Kseniia-Artemova@users.noreply.github.com>
2023-09-07 18:51:21 +0200

    organized controller caching, low-level caching and full caching; main environment variables added to .env; added service function get_categories_cache (#26)

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from django.views.decorators.cache import cache_page, never_cache
 from blog.views import BlogEntryListView, BlogEntryCreateView, BlogEntryUnpublishedListView
 from blog.views import BlogEntryDetailView, publish_blog_entry, BlogEntryUpdateView, BlogEntryDeleteView
 from blog.apps import BlogConfig
@@ -7,11 +7,11 @@ from blog.apps import BlogConfig
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path('blog/', BlogEntryListView.as_view(), name='blog'),
+    path('blog/', cache_page(60)(BlogEntryListView.as_view()), name='blog'),
     path('unpublished_entries/', BlogEntryUnpublishedListView.as_view(), name='unpublished_entries'),
-    path('new_entry/', BlogEntryCreateView.as_view(), name='new_entry'),
-    path('entry/<slug:slug>/', BlogEntryDetailView.as_view(), name='blog_entry'),
-    path('activity/<slug:slug>/', publish_blog_entry, name='publish'),
-    path('update_entry/<slug:slug>/', BlogEntryUpdateView.as_view(), name='update_entry'),
-    path('delete_entry/<slug:slug>/', BlogEntryDeleteView.as_view(), name='delete_entry'),
+    path('new_entry/', never_cache(BlogEntryCreateView.as_view()), name='new_entry'),
+    path('entry/<slug:slug>/', cache_page(60)(BlogEntryDetailView.as_view()), name='blog_entry'),
+    path('activity/<slug:slug>/', never_cache(publish_blog_entry), name='publish'),
+    path('update_entry/<slug:slug>/', never_cache(BlogEntryUpdateView.as_view()), name='update_entry'),
+    path('delete_entry/<slug:slug>/', never_cache(BlogEntryDeleteView.as_view()), name='delete_entry'),
 ]
